eb3821844142ea3ab94e532b58585d35ccb7e8d9
Emmanuel-Kenga <123498916+Emmanuel-Kenga@users.noreply.github.com>
2024-03-08 13:36:06 +0300

    Add files via upload
    A python program that takes in user inputs them print them out. PLP program

---
+++ flow.py
@@ -0,0 +1,60 @@
+
+#for loop
+# words=["one" "two" "three"]
+# for x in words:
+#     print(x)
+
+#List datastructure- it's mutable
+#languages=["swift", "java", "js"]
+#print(languages[0])
+
+#Tuple DS- it's immutable
+#product=('microsoft', 'xbox', 55.9)
+#print(product)
+
+#Set DS-
+# stid={111,212,331,432,543}
+# print(stid)
+# print(type(stid))
+
+#Dictionary DS
+# cc={'napel':'kathmandu', 'italy':'rome', 'england':'london'}
+# print(cc)
+
+#type conversion- implicit convversion
+# int=123
+# float=1.23
+# nn=int + float
+# print("value:", type(nn))
+
+#explicit conversion
+# num_string='12'
+# num_integer=23
+# print("data type of num_string before type casting:", type(num_string))
+# #explicit type conversion
+# num_string=int(num_string)
+
+# num_sum=num_integer + num_string
+# print("sum:", num_sum)
+# print("data type of num_sum:", type(num_sum))
+
+#printing out and collecting user input 
+#print(object= separator= end= file= flush=)- syntax 
+# print('Good Morning', end='')# concatination using end
+# print('it is rainy today')
+
+#print Concatination Strings
+#print('powerLP ' + 'awesome')
+
+#Python User Input
+# nun=input('enter number')
+# print('you entered:', nun)
+# print('data type of num:', type(nun))
+
+#Assignment 1
+name=input('enter your name:')
+age=input('enter your age:')
+location=input('enter your location:')
+print('Below is my details:')
+print('Hello',name + 'you are',age + 'years old and live in', location)
+
